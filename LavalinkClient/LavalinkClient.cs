1d81b82724b8ab3ae305636076fc1df016e8fd06
Nicolas Van Damme <nicolas.vandamme@student.hogent.be>
2021-04-01 20:37:31 +0200

    Added client project for Lavalink

---
+++ LavalinkClient/LavalinkClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LavalinkClient
+{
+    class LavalinkClient
+    {
+        public LavalinkClient()
+        {
+        }
+
+        public async Task StartAsync()
+        {
+            var jarLocation = AppDomain.CurrentDomain.BaseDirectory + "/Lavalink/Lavalink.jar";
+        }
+    }
+}
